6ea67c549b758a4f846d500589e8609964b603e1
CairX <lazycairx@gmail.com>
2015-07-15 20:55:06 +0200

    Handle one simple leveled each-loop.

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ def clean(variable):
     return variable[2:-2]
 
 
+def each(data, template):
+    pattern = "({{#each (.*?)}}(.*?){{/each}})"
+    all_each = re.findall(pattern, template, re.DOTALL)
+
+    one_each = all_each[0]
+    one_each_data = data[one_each[1]]
+    one_each_template = one_each[2]
+    one_each_original = one_each[0]
+    one_each_result = ""
+
+    for item in one_each_data:
+        one_each_result += replace(item, one_each_template)
+
+    return template.replace(one_each_original, one_each_result, 1)
+
+
 def replace(data, template):
     variables = re.findall("{{.*?}}", template)
 
@@ -16,7 +32,15 @@ def replace(data, template):
 
 
 if __name__ == "__main__":
-    with open("data/one.json", "r") as data_file, open("data/one.html") as template_file:
+    # data_path = "data/one.json"
+    # template_path = "data/one.html"
+
+    data_path = "data/collection.json"
+    template_path = "data/collection.html"
+
+    with open(data_path) as data_file, open(template_path) as template_file:
         data = json.load(data_file)
         template = template_file.read()
-        print(replace(data, template))
+
+        print(each(data, template))
+        # print(replace(data, template))

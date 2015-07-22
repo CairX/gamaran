4ca42e68fb881d809f6c331bdec5335383409dec
CairX <lazycairx@gmail.com>
2015-07-22 14:17:09 +0200

    Split test run and parser.

--- parser.py
+++ parser.py
@@ -1,6 +1,4 @@
-import json
 import re
-import time
 
 debug = False
 
@@ -12,18 +10,6 @@ def log(string):
 
 def clean(variable):
     return variable[2:-2]
-
-
-def compare(expected, s):
-    l1 = expected.splitlines()
-    l2 = s.splitlines()
-
-    for i in range(len(l1)):
-        if l1[i] != l2[i]:
-            print("Line " + str(i + 1))
-            print("Expected: " + repr(l1[i]))
-            print("Was: " + repr(l2[i]))
-            break
 
 
 def variables(data, template):
@@ -110,49 +96,3 @@ def scope(data, template, level):
     template = variables(data, template)
 
     return template, index
-
-
-def run_test(package, test):
-    # Start a timer for execution time.
-    start = time.clock()
-
-    # Paths
-    data_path = "data/{0}/{1}/data.json".format(package, test)
-    template_path = "data/{0}/{1}/template.html".format(package, test)
-    expected_path = "data/{0}/{1}/expected.html".format(package, test)
-    result_path = "data/{0}/{1}/result.html".format(package, test)
-
-    with open(data_path) as data_file, open(template_path) as template_file:
-        data = json.load(data_file)
-        template = template_file.read()
-
-        # Initiate the parsen with the global as the most outer scope.
-        result, index = scope(data, template, 0)
-
-        # Store the result for easier viewing for cases when a test fails.
-        with open(result_path, "w") as result_file:
-            result_file.write(result)
-            print("Result stored: " + result_path)
-
-        # Validate the test.
-        with open(expected_path) as expected_file:
-            expected = expected_file.read()
-            print("")
-            if result == expected:
-                print(package + "/" + test + ": Passed")
-            else:
-                print(package + "/" + test + ": Failed")
-                compare(expected, result)
-
-    print("Time: " + str(time.clock() - start))
-
-
-if __name__ == "__main__":
-    packages = {
-        "global": ["single"],
-        "each": ["collection", "empty", "multiple", "nested", "this"]
-    }
-
-    for package, tests in packages.items():
-        for test in tests:
-            run_test(package, test)

15f52657bd35a1653f21c1560a2cd64f1b3ee1da
CairX <lazycairx@gmail.com>
2015-09-19 12:35:48 +0200

    Add argument parser.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import sys
 
@@ -6,9 +7,34 @@ from parser import parse
 
 
 if __name__ == "__main__":
-    template_path = sys.argv[1]
-    data_path = sys.argv[2]
-    result_path = sys.argv[3]
+    parser = argparse.ArgumentParser(
+        prog="gamaran",
+        description="Static content generator.")
+
+    parser.add_argument(
+        "template",
+        nargs=1,
+        help="path to template file")
+
+    parser.add_argument(
+        "data",
+        nargs=1,
+        help="path to data file")
+
+    parser.add_argument(
+        "result",
+        nargs=1,
+        help="path to result file")
+
+    parser.add_argument(
+        "-v", "--version",
+        action="version",
+        version='%(prog)s 1.0')
+
+    args = parser.parse_args()
+    template_path = args.template[0]
+    data_path = args.data[0]
+    result_path = args.result[0]
 
     with open(template_path) as template_file:
         template = template_file.read()
